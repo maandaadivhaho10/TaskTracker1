9312ee75015270167b09e75a12a84a78f51fbda8
adivhaho <maandaadivhaho74@gmail.com>
2025-12-26 02:02:45 +0200

    Allow Azure host in Vite preview

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT,
+    allowedHosts: ['adivhaho-recipefinder.azurewebsites.net']
+  }
 })
