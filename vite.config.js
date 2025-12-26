0a1c4be6ec4553483fe7cd4d2e377cb887cfaf2e
adivhaho <maandaadivhaho74@gmail.com>
2025-12-27 00:21:50 +0200

    changed port and allowhost

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT,
-    allowedHosts: ['adivhaho-recipefinder.azurewebsites.net']
+    allowedHosts: ['taskme.azurewebsites.net']
   }
 })
