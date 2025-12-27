7439b9b5e96e71183e3961cead2045571d65e771
adivhaho <maandaadivhaho74@gmail.com>
2025-12-27 02:05:28 +0200

    added correct url for azure

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT,
-    allowedHosts: ['taskme-user2025.azurewebsites.net']
+    allowedHosts: ['adiedeployements.azurewebsites.net']
   }
 })
